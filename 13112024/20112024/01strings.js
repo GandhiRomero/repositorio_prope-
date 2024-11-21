4532b8b6be28a2e3371de5cfdc98d76cc536973b
gandhi romero <gandhiromero@yahoo.com>
2024-11-20 21:20:21 -0600

    prueba

---
+++ 13112024/20112024/01strings.js
@@ -0,0 +1,14 @@
+function separanombrenombrecompleto(){ 
+    solicitatNombrecompleto();
+    console.log("el nombre compleo es "+ resultado);
+}
+
+function invertircadena (){
+    solicitatNombrecompleto();
+    cadenainvertida=cadenaseparada.reverse();
+console.log("la cadena invertida es"+cadenainvertida);
+
+    }
+    //Invocacion
+    //mostrarnombrecompleto();
+    
